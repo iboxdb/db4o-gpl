4772ddb91fa1ed69dfe6de101f523dd645af9fa9
Bruce <bruce@localhost_address.com>
2019-08-26 22:06:04 -0400

    Tutorial

---
+++ db4o.net/Db4odoc.Tutorial.Chapters/App.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Db4objects.Db4o.Tutorial.Console
+{
+    class App
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
+            Db4odoc.Tutorial.F1.Chapter1.FirstStepsExample.Main(args);
+            Db4odoc.Tutorial.F1.Chapter2.StructuredExample.Main(args);
+            Db4odoc.Tutorial.F1.Chapter3.OMEExample.Main(args);
+            Db4odoc.Tutorial.F1.Chapter4.CollectionsExample.Main(args);
+            Db4odoc.Tutorial.F1.Chapter5.InheritanceExample.Main(args);
+           
+            Db4odoc.Tutorial.F1.Chapter8.TransparentActivationExample.Main(args);
+            Db4odoc.Tutorial.F1.Chapter9.TransparentPersistenceExample.Main(args);
+            
+            System.Console.Error.WriteLine("Client-Server");
+            Db4odoc.Tutorial.F1.Chapter6.ClientServerExample.Main(args);
+            System.Console.WriteLine("End");
+        }
+    }
+}

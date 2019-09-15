1a2375b5237e4223ca2df1785e4b5e654f82606c
Bruce <bruce@localhost_address.com>
2019-09-15 02:16:42 -0400

    OME Java

---
+++ admins/om.j/ObjectManager/src/objectmanager/ObjectManager.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objectmanager;
+
+/**
+ *
+ * @author user
+ */
+public class ObjectManager {
+
+ /*
+Build this Project
+
+compile:
+Created dir: /home/user/github/db4o-gpl/admins/om.j/ObjectManager/dist
+Copying 1 file to /home/user/github/db4o-gpl/admins/om.j/ObjectManager/build
+Copy libraries to /home/user/github/db4o-gpl/admins/om.j/ObjectManager/dist/lib.
+Building jar: /home/user/github/db4o-gpl/admins/om.j/ObjectManager/dist/ObjectManager.jar
+    
+    
+To run this application from the command line without Ant, try:
+java -jar "/home/user/github/db4o-gpl/admins/om.j/ObjectManager/dist/ObjectManager.jar"
+
+    
+ */
+    
+    public static void main(String[] args) { 
+        com.db4o.objectManager.v2.Dashboard.main(args);
+    }
+    
+}

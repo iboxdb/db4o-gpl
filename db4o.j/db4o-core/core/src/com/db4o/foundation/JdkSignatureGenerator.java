c022d135da002adbe0d26a6414cf562f15dfeadc
Bruce <bruce@localhost_address.com>
2019-09-20 01:31:27 -0400

    Signature

--- db4o.j/db4o-core/core/src/com/db4o/foundation/JdkSignatureGenerator.java
+++ db4o.j/db4o-core/core/src/com/db4o/foundation/JdkSignatureGenerator.java
@@ -1,52 +1,52 @@
 /* Copyright (C) 2004 - 2012  Versant Inc.  http://www.db4o.com */
-
 package com.db4o.foundation;
 
-import java.net.*;
+//import java.net.*;
 import java.util.*;
 
 /**
- * @exclude
- * @sharpen.ignore
+ * @exclude @sharpen.ignore
  */
 public class JdkSignatureGenerator {
-	
-	private static final Random _random = new Random();
-	
-	private static int _counter;
-	
-	public static String generateSignature() {
-		StringBuffer sb = new StringBuffer();
-		try {
-			String hostName = java.net.InetAddress.getLocalHost().getHostName() + "_";
-			if(hostName.length() > 15){
-				hostName = hostName.substring(0,15);
-			}
-			sb.append(hostName);
-		} catch (UnknownHostException e) {
-		}
-		sb.append(Long.toHexString(System.currentTimeMillis()));
-		sb.append(Integer.toHexString(_counter++));
-		int hostAddress = 0;
-		byte[] addressBytes;
-		try {
-			addressBytes = java.net.InetAddress.getLocalHost().getAddress();
-			for (int i = 0; i < addressBytes.length; i++) {
-				hostAddress <<= 4;
-				hostAddress -= addressBytes[i];
-			}
-		} catch (UnknownHostException e) {
-		}
-		sb.append(Integer.toHexString(hostAddress));
-		for (int i = 0; i < 4; i++) {
-			sb.append(Integer.toHexString(randomInt()));
-		}
-		return sb.toString().substring(0, 31);
-	}
 
-	private static int randomInt() {
-		return _random.nextInt();
-	}
+    private static final Random _random = new Random();
 
+    private static int _counter;
+
+    public static String generateSignature() {
+        StringBuffer sb = new StringBuffer();
+        try {
+            //String hostName = java.net.InetAddress.getLocalHost().getHostName() + "_";
+            UUID uid = UUID.randomUUID();
+            String hostName = "H" + uid.getLeastSignificantBits() + "_" + uid.getMostSignificantBits();
+            if (hostName.length() > 15) {
+                hostName = hostName.substring(0, 15);
+            }
+            sb.append(hostName);
+        } catch (Throwable e) {
+        }
+        sb.append(Long.toHexString(System.currentTimeMillis()));
+        sb.append(Integer.toHexString(_counter++));
+        int hostAddress = 0;
+
+        try {
+            byte[] addressBytes = new byte[]{(byte) randomInt(), (byte) randomInt(), (byte) randomInt(), (byte) randomInt()};
+            //addressBytes = java.net.InetAddress.getLocalHost().getAddress();
+            for (int i = 0; i < addressBytes.length; i++) {
+                hostAddress <<= 4;
+                hostAddress -= addressBytes[i];
+            }
+        } catch (Throwable e) {
+        }
+        sb.append(Integer.toHexString(hostAddress));
+        for (int i = 0; i < 4; i++) {
+            sb.append(Integer.toHexString(randomInt()));
+        }
+        return sb.toString().substring(0, 31);
+    }
+
+    private static int randomInt() {
+        return _random.nextInt();
+    }
 
 }
--- db4o.j/db4o-core/src/com/db4o/Empty.java
+++ db4o.j/db4o-core/src/com/db4o/Empty.java
@@ -1,5 +1,29 @@
 package com.db4o;
 
+import com.db4o.config.annotations.Indexed;
+import java.io.File;
+
 public class Empty {
     //Runfile: com.db4odoc.f1.Main.java 
+/*
+    public static class Node {
+
+        @Indexed
+        public String name;
+        public Node Left;
+        public Node Right; ;
+    }
+*/
+    public static void main(String[] args) {
+/*
+        String dbname = "/tmp/node.j.db";
+        new File(dbname).delete();
+        try (EmbeddedObjectContainer x = Db4oEmbedded.openFile(dbname)) {
+            Node n = new Node();
+            n.name = "CCC";
+            x.store(n);
+            x.commit();
+        }
+*/
+    }
 }
